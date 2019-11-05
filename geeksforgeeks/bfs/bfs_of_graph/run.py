ba0e686e2e8bfe7ee2a85d248f8715b8c043dbe2
Chris Davie <cjdavie@googlemail.com>
2019-11-05 20:48:01 +0000

    passing tests and other test cases

--- geeksforgeeks/bfs/bfs_of_graph/run.py
+++ geeksforgeeks/bfs/bfs_of_graph/run.py
@@ -1,14 +1,29 @@
-import atexit
-import io
-import sys
 from collections import defaultdict
 from typing import DefaultDict, List, Tuple
-import queue
+from queue import Queue
 
 
 def bfs(graph: DefaultDict[int, List[int]], num_nodes: int) -> List[int]:
 
-    return [-1]
+    result = []
+    seen = defaultdict(bool)
+
+    q = Queue()
+    q.put(0)
+    seen[0] = True
+
+    while not q.empty():
+
+        node_id = q.get()
+        print(node_id, end=" ")
+        result.append(node_id)
+
+        for child_id in graph[node_id]:
+            if not seen[child_id]:
+                q.put(child_id)
+                seen[child_id] = True
+
+    return result
 
 
 def initialise_graph(edges: List[Tuple[int]]) -> DefaultDict[int, List[int]]:
--- geeksforgeeks/bfs/bfs_of_graph/test.py
+++ geeksforgeeks/bfs/bfs_of_graph/test.py
@@ -22,3 +22,32 @@ class TestExamples(TestCase):
         graph = initialise_graph(edges)
 
         self.assertEqual(bfs(graph, num_nodes), [0, 1, 2])
+
+    def test_more_nodes_than_edges(self):
+
+        num_nodes = 6
+        edges = [(0, 1)]
+
+        graph = initialise_graph(edges)
+
+        self.assertEqual(bfs(graph, num_nodes), [0, 1])
+
+    def test_circular(self):
+        # it's a graph not a tree!
+
+        num_nodes = 2
+        edges = [(0, 1), (1, 0)]
+
+        graph = initialise_graph(edges)
+
+        self.assertEqual(bfs(graph, num_nodes), [0, 1])
+
+    def test_circular_not_root(self):
+        # it's a graph not a tree!
+
+        num_nodes = 2
+        edges = [(0, 1), (1, 2), (2, 1)]
+
+        graph = initialise_graph(edges)
+
+        self.assertEqual(bfs(graph, num_nodes), [0, 1, 2])
