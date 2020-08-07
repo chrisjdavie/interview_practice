f4472a6323b9b8685ea0daaf9da3d6b9fba25a0b
Chris Davie <cjdavie@googlemail.com>
2020-08-07 16:45:26 +0100

    clean up roman numerals

--- leetcode/0013-roman-numerals/fiddling.py
+++ leetcode/0013-roman-numerals/fiddling.py
@@ -1,9 +1,10 @@
+from typing import Dict
 from unittest import TestCase
 
 from parameterized import parameterized
 
 
-NUMERAL_INT = {
+NUMERAL_INT: Dict[str, int] = {
     "I": 1,
     "V": 5,
     "X": 10,
@@ -18,7 +19,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
 
         s_rev = s[::-1]
-        running_total = NUMERAL_INT[s_rev[0]]
+        running_total: int = NUMERAL_INT[s_rev[0]]
 
         for num_0, num_1 in zip(s_rev[:-1], s_rev[1:]):
             if NUMERAL_INT[num_0] <= NUMERAL_INT[num_1]:
@@ -41,20 +42,19 @@ class TestRomanToInt(TestCase):
         ("MCM", 1900),
         ("MCMXIV", 1914)
     ])
-    def test_goes(self, NUMERAL_INT, integers):
+    def test_works(self, numeral, integers):
 
         solution = Solution()
-        self.assertEqual(solution.romanToInt(NUMERAL_INT), integers)
+        self.assertEqual(solution.romanToInt(numeral), integers)
 
-    # @parameterized.expand([
-    #     ("III", 3),
-    #     ("IV", 4),
-    #     ("IX", 9),
-    #     ("LVIII", 58),
-    #     ("MCMXIV", 1994)
-    # ])
-    # def test_examples(self, NUMERAL_INT, integers):
+    @parameterized.expand([
+        ("III", 3),
+        ("IV", 4),
+        ("IX", 9),
+        ("LVIII", 58)
+    ])
+    def test_examples(self, numeral, integers):
 
-    #     solution = Solution()
+        solution = Solution()
 
-    #     self.assertEqual(solution.romanToInt(NUMERAL_INT), integers)
+        self.assertEqual(solution.romanToInt(numeral), integers)
