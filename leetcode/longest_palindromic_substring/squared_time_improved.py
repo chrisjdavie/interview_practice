85a4f9546cc606c4c5bcd432fb24d9c9f9eb5ae2
Chris Davie <cjdavie@googlemail.com>
2022-10-14 17:00:59 +0100

    Leetcode, longest palindromic substring, squared time

--- leetcode/longest_palindromic_substring/squared_time_improved.py
+++ leetcode/longest_palindromic_substring/squared_time_improved.py
@@ -1,3 +1,10 @@
+"""
+The main improvement here is understanding the slicing is an O(N) operation, so slicing after finding the indicies adds no complexity
+
+It does take twice as long thou
+
+Inspiration from solutions displayed in the chat
+"""
 from itertools import chain
 from typing import Optional
 from unittest import TestCase
@@ -8,10 +15,10 @@ from parameterized import parameterized
 class Solution():
 
     @staticmethod
-    def _longestPalindromeFromIndicies(i: int, j: int, s: str) -> Optional[tuple[int, int]]:
+    def _longestPalindromeFromIndicies(i: int, j: int, s: str) -> str:
 
-        i_longest = None
-        j_longest = None
+        i_longest = 0
+        j_longest = -1
 
         for i, j in zip(range(i, -1, -1), range(j, len(s))):
             if s[i] != s[j]:
@@ -19,48 +26,42 @@ class Solution():
             i_longest = i
             j_longest = j
 
-        if i_longest is not None:
-            return (i_longest, j_longest)
-        return None
+        return s[i_longest:j_longest+1]
 
     def longestPalindrome(self, s: str) -> str:
 
-        i_longest = 0
-        j_longest = 0
-        len_longest = 0
+        longest_palindrome = ""
 
-        for i, j in chain(zip(range(len(s)), range(len(s))), zip(range(len(s)-1), range(1,len(s)))):
-            if inds := self._longestPalindromeFromIndicies(i, j, s):
-                i_cand = inds[0]
-                j_cand = inds[1]
-                if j_cand - i_cand + 1 > len_longest:
-                    i_longest = i_cand
-                    j_longest = j_cand
-                    len_longest = j_cand - i_cand + 1 
+        for i, j in chain(
+                zip(range(len(s)), range(len(s))), 
+                zip(range(len(s)-1), range(1,len(s)))
+            ):
+            if len(cand := self._longestPalindromeFromIndicies(i, j, s)) > len(longest_palindrome):
+                longest_palindrome = cand
 
-        return s[i_longest:j_longest+1]
+        return longest_palindrome
 
 
 class TestSolution(TestCase):
 
     @parameterized.expand([
-        (0, 0, (0, 0)),
-        (1, 1, (0, 2)),
-        (2, 2, (1, 3)),
-        (3, 3, (3, 3)),
-        (4, 4, (4, 4)),
+        (0, 0, "b"),
+        (1, 1, "bab"),
+        (2, 2, "aba"),
+        (3, 3, "a"),
+        (4, 4, "d"),
     ])
     def testLongestPalindromeFromIndiciesSameStart(self, i, j, expected_output):
 
-        self.assertTupleEqual(
+        self.assertEqual(
             Solution._longestPalindromeFromIndicies(i, j, "babad"),
             expected_output
         )
 
     @parameterized.expand([
-        (0, 1, None),
-        (1, 2, (1, 2)),
-        (2, 3, None),
+        (0, 1, ""),
+        (1, 2, "bb"),
+        (2, 3, ""),
     ])
     def testLongestPalindromeFromIndiciesDiffStart(self, i, j, expected_output):
 
