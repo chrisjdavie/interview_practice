d1c1752234cb2c4dce010240be32c779c3d76866
Chris Davie <cjdavie@googlemail.com>
2022-10-20 11:47:56 +0100

    Leetcode, palindrome number, using strings

---
+++ leetcode/palindrome_number/strings.py
@@ -0,0 +1,17 @@
+import pytest
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        for lhs, rhs in zip(str(x), str(x)[::-1]):
+            print(lhs, rhs)
+            if lhs != rhs:
+                return False
+        return True
+
+@pytest.mark.parametrize(
+    "test_number,expected",
+    ((121, True), (123, False))
+)
+def test(test_number, expected):
+    assert Solution().isPalindrome(test_number) == expected
+
