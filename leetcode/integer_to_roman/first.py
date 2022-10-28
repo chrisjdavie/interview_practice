a11374db7d60cc88a509bcacfa28c030f8305754
Chris Davie <cjdavie@googlemail.com>
2022-10-28 16:36:32 +0100

    Leetcode, integer to roman, bad solution but works

--- leetcode/integer_to_roman/first.py
+++ leetcode/integer_to_roman/first.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 
@@ -12,16 +14,41 @@ INTEGERS_TO_NUMERALS = {
     1000: "M",
 }
 
+EXCEPTIONS = {
+    4: "IV",
+    9: "IX",
+    40: "XL",
+    90: "XC",
+    400: "CD",
+    900: "CM",
+}
+
+
 class Solution:
 
     def _intToRoman(self, num: int, key_nums: list[int]) -> str:
+        exception_numeral: str = ""   
+        floor_div_numeral: str = ""
+        remain_numeral: str = ""
+
+        rank = 10**(math.floor(math.log10(num)))
+        leading_num = (num//rank) * rank
+        if leading_num in EXCEPTIONS:
+            exception_numeral: str = EXCEPTIONS[leading_num]
+            num -= leading_num
+
         this_key: int = key_nums.pop()
-        denom: int = num//this_key
+
+        floor_div: int = num//this_key
         remain: int = num%this_key
+
         remain_numeral: str = ""
         if remain:
             remain_numeral: str = self._intToRoman(remain, key_nums)
-        return INTEGERS_TO_NUMERALS[this_key]*denom + remain_numeral
+
+        floor_div_numeral = INTEGERS_TO_NUMERALS[this_key]*floor_div        
+
+        return exception_numeral + floor_div_numeral + remain_numeral
 
 
     def intToRoman(self, num: int) -> str:
@@ -36,6 +63,11 @@ class Solution:
         (13,"XIII"),
         (27,"XXVII"),
         (1627, "MDCXXVII"),
+        (104, "CIV"),
+        (409, "CDIX"),
+        (1994, "MCMXCIV"),
+        (58, "LVIII"),
+        (143, "CXLIII"),
     )
 )
 def test(test_int, expected_roman):
