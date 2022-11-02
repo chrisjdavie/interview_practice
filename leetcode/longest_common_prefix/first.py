a2b4a4abb6817fc2e45e74eb0f0c6af3a1e829bc
Chris Davie <cjdavie@googlemail.com>
2022-11-02 10:20:59 +0000

    Leetcode, longest common prefix, first solution

--- leetcode/longest_common_prefix/first.py
+++ leetcode/longest_common_prefix/first.py
@@ -3,8 +3,14 @@ import pytest
 
 class Solution:
 
-	def longestCommonPrefix(self, strs: list[str]) -> str:
-	    return "~"
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        longest_common_prefix = ""
+        for chars in zip(*strs):
+            if all(chars[0] == c for c in chars[1:]):
+                longest_common_prefix += chars[0]
+            else:
+                break
+        return longest_common_prefix
 
 
 @pytest.mark.parametrize(
@@ -12,6 +18,7 @@ class Solution:
     (
         (["flower","flow","flight"],"fl"),
         (["dog","racecar","car"], ""),
+        (["ded", "dad"], "d"),
     )
 )
 def test(words, expected_prefix):
