95c59644d6a1a48786cafc166ef924151d93fd48
Chris Davie <cjdavie@googlemail.com>
2022-11-22 11:11:38 +0000

    Leetcode, remove element, using filter

--- leetcode/remove_element/filter.py
+++ leetcode/remove_element/filter.py
@@ -1,4 +1,9 @@
 """
+I think this is *probably* improved, it's def terser and uses filter and such, but the
+first version is more explicit, probably have to think less hard to understand it. But not
+much
+---------------------------------
+
 Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
@@ -14,12 +19,10 @@ import pytest
 
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        ind_filtered: int = 0
-        for n in nums:
-            if n != val:
-                nums[ind_filtered] = n
-                ind_filtered += 1
-        return ind_filtered
+        i = -1
+        for i, n in enumerate(filter(lambda x: x != val, nums)):
+            nums[i] = n
+        return i + 1
 
 
 @pytest.mark.parametrize(
