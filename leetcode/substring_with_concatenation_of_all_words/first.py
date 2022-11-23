1fc91d663166489e7dbb8fe80cbf8e9cde3b2896
Chris Davie <cjdavie@googlemail.com>
2022-11-23 16:35:25 +0000

    Leetcode, substring with concatenation of all words, subset of failing tests

---
+++ leetcode/substring_with_concatenation_of_all_words/first.py
@@ -0,0 +1,38 @@
+"""
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+
+    For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+
+Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+-----------------------------------
+A solution for this would be a Trie + a counter, so I'll give that a go when I'm next doing this
+"""
+import pytest
+
+
+class Solution:
+    def findSubstring(self, s: str, words: list[str]) -> list[int]:
+        return [-1]
+
+
+@pytest.mark.parametrize(
+    "input_string,words,expected_indicies",
+    (
+        ("barfoothefoobarman", ["foo","bar"], [0,9]),
+        ("wordgoodgoodgoodbestword", ["word","good","best","word"], [])
+    )
+)
+def test_leetcode(input_string, words, expected_indicies):
+    assert Solution().findSubstring(input_string, words) == expected_indicies
+
+"""
+Can convert the following into a test using itertools.permutations and then some magic
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+"""
+
+
