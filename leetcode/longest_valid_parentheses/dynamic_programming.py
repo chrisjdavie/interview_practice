45556f01e762d6a98fc027249bd6dacaf840797b
Chris Davie <cjdavie@googlemail.com>
2023-03-17 15:02:14 +0000

    Leetcode, longest valid parentheses, dynamic programming, try two, works

--- leetcode/longest_valid_parentheses/dynamic_programming.py
+++ leetcode/longest_valid_parentheses/dynamic_programming.py
@@ -1,10 +1,29 @@
+from typing import Optional
+
 import pytest
 
 
 class Solution:
 
     def longestValidParentheses(self, s: str) -> int:
-        return len(s)
+
+        dp_ind_open: list[int] = [None]*len(s)
+        i_open: Optional[int] = None
+
+        for i_close, brace in enumerate(s):
+            if brace == ")":
+                if dp_ind_open[i_close - 1] is not None:
+                    i_open = dp_ind_open[i_close - 1] - 1
+                else:
+                    i_open = i_close - 1
+
+                if i_open >= 0 and s[i_open] == "(":
+                    if dp_ind_open[i_open - 1] is not None:
+                        dp_ind_open[i_close] = dp_ind_open[i_open - 1]
+                    else:
+                        dp_ind_open[i_close] = i_open
+
+        return max([0,] + [i_close - i_open + 1 for i_close, i_open in enumerate(dp_ind_open) if i_open is not None])
 
 
 @pytest.mark.parametrize(
@@ -12,7 +31,11 @@ class Solution:
     (
         ("", 0),
         ("()", 2),
-        (")()", 2)
+        (")()", 2),
+        ("(())", 4),
+        ("()()", 4),
+        ("(()())", 6),
+        ("))", 0)
     )
 )
 def test_longestValidParentheses(braces, length_valid):
