444f94cc66b0049ca2a78b08a97e22b74f311c71
Chris Davie <cjdavie@googlemail.com>
2023-04-04 13:29:48 +0100

    Leetcode, longest valid parentheses, stack approach working

--- leetcode/longest_valid_parentheses/stack_approach.py
+++ leetcode/longest_valid_parentheses/stack_approach.py
@@ -5,14 +5,31 @@ class Solution:
 
     def longestValidParentheses(self, s: str) -> int:
 
-        return 0
+        max_len: int = 0
+        opening_stack: list[int] = [-1] # -1 the starting point of current set of braces
+
+        for ind, paren in enumerate(s):
+            if paren == "(":
+                opening_stack.append(ind)
+            else:
+                opening_stack.pop()
+                if opening_stack:
+                    max_len = max((ind - opening_stack[-1], max_len))
+                else:
+                    opening_stack.append(ind)
+
+        return max_len
 
 
 @pytest.mark.parametrize(
     "braces,expected_length",
     (
         ("", 0),
-        ("()", 2)
+        ("()", 2),
+        ("(()", 2),
+        (")()", 2),
+        ("(()))()", 4),
+        ("(()())", 6)
     )
 )
 def test_longestValidParentheses(braces, expected_length):
