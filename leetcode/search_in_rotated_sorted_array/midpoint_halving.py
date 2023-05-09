774ff8721c021f4785eb93872eac778477f1ae9d
Chris Davie <cjdavie@googlemail.com>
2023-05-09 16:59:55 +0100

    Leetcode, search in rotated sorted array, midpoint halving working but still very slow

--- leetcode/search_in_rotated_sorted_array/midpoint_halving.py
+++ leetcode/search_in_rotated_sorted_array/midpoint_halving.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
+import bisect
+
 import pytest
 
 
@@ -7,17 +9,43 @@ class Solution:
     @staticmethod
     def _find_pivot_index(nums: list[int]) -> int:
 
-        return -1
+        lo = 0
+        hi = len(nums) - 1
+
+        if len(nums) == 1 or nums[lo] < nums[hi]:
+            return 0
+
+        while nums[lo + 1] > nums[lo]:
+            mid = (lo + hi)//2
+            if nums[mid] > nums[lo]:
+                lo = mid
+            else:
+                hi = mid
+        return lo + 1
 
     def search(self, nums: list[int], target: int) -> int:
 
-        return 0
+        pivot = self._find_pivot_index(nums)
+        if nums[0] <= target:
+            index = bisect.bisect_left(nums, target, hi=pivot)
+        else:
+            index = bisect.bisect_left(nums, target, lo=pivot)
+
+        if index < len(nums) and nums[index] == target:
+            return index
+        return -1
 
 
 @pytest.mark.parametrize(
     "nums,pivot_index",
     (
-        ([4,1,2], 1),
+        ([0],0),
+        ([0,1],0),
+        ([4,1,2],1),
+        ([3,4,1,2],2),
+        ([3,4,5,0,1],3),
+        ([4,5,6,7,0,1,2], 4),
+        ([3,4,5,6,7,1], 5),
     )
 )
 def test_find_pivot(nums, pivot_index):
@@ -28,7 +56,15 @@ def test_find_pivot(nums, pivot_index):
 @pytest.mark.parametrize(
     "nums,target,expected_result",
     (
+        ([4, 5, 6, 1, 2, 3], 5, 1),
+        ([4, 5, 6, 1, 2, 3], 2, 4),
         ([0], 0, 0),
+        ([0], 1, -1),
+        ([4,5,6,7,0,1,2],0,4),
+        ([4,5,6,7,0,1,2],5,1),
+        ([4,5,6,7,0,1,2],3,-1),
+        ([1,3], 3, 1),
+        ([3,1], 3, 0),
     ),
 )
 def test_search(nums, target, expected_result):
